026423aa37ffa3b63ee42687959ec7d9b1f70fca
prenuevos <prenuevos@Wellick.ac.labf.usb.ve>
2018-05-11 10:37:15 -0400

    Started the main.py file with the Token names and definitions

---
+++ main.py
@@ -0,0 +1,106 @@
+import ply.ply.lex as lex
+import sys
+import re
+
+# List of token names
+tokens = (
+	'TkComa',
+	'TkPunto',
+	'TkDosPuntos',
+	'TkParAbre',
+	'TkParCierra',
+	'TkCorcheteAbre',
+	'TkCorcheteCierra',
+	'TkLlaveAbre',
+	'TkLlaveCierra',
+	'TkHacer',
+	'TkAsignacion',
+	'TkSuma',
+	'TkResta',
+	'TkMult',
+	'TkDiv',
+	'TkMod',
+	'TkConjuncion',
+	'TkDisyuncion',
+	'TkNegacion',
+	'TkMenor',
+	'TkMenorIgual',
+	'TkMayor',
+	'TkMayorIgual',
+	'TkIgual',
+	'TkSiguienteCar',
+	'TkAnteriorCar',
+	'TkValorAscii',
+	'TkConcatenacion',
+	'TkShift',
+	'NUMBER'
+)
+
+# Regular expression rules for simple tokens
+t_TkComa  = r','
+t_TkPunto = r'\.'
+t_TkDosPuntos = r':'
+t_TkParAbre = r'\('
+#t_TkParCierra = r')'
+t_TkCorcheteAbre  = r'\['
+t_TkCorcheteCierra  = r'\]'
+t_TkLlaveAbre = r'\{'
+t_TkLlaveCierra = r'\}'
+t_TkHacer = r'->'
+t_TkAsignacion = r'<-'
+t_TkSuma  = r'\+'
+t_TkResta = r'-'
+t_TkMult  = r'\*'
+t_TkDiv = r'/'
+t_TkMod = r'%'
+#t_TkConjuncion  = r'[/][\]'
+t_TkDisyuncion  = r'[\][/]'
+t_TkNegacion  = r'not'
+t_TkMenor = r'<'
+t_TkMenorIgual  = r'<='
+t_TkMayor = r'>'
+t_TkMayorIgual  = r'>='
+t_TkIgual = r'='
+t_TkSiguienteCar  = r'[+][+]'
+t_TkAnteriorCar = r'--'
+#t_TkValorAscii  = r'#'
+t_TkConcatenacion = r'::'
+t_TkShift = r'\$'
+
+
+# A regular expression rule with some action code
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)    
+    return t
+
+# Define a rule so we can track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+
+# Error handling rule
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex()
+
+# Test it out
+data = '''
+3 $ 4
+'''
+
+# Give the lexer some input
+lexer.input(data)
+
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok: 
+        break      # No more input
+    print(tok)
