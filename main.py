2da5dfeb425fb5c3a671c243e4d478bb9d242aa7
Sandra <sandrav0609@gmail.com>
2018-05-13 15:34:17 -0400

    Fix char literal regex. Add print for tokens or errors

--- main.py
+++ main.py
@@ -80,8 +80,8 @@ t_TkResta = r'-'
 t_TkMult  = r'\*'
 t_TkDiv = r'/'
 t_TkMod = r'%'
-#t_TkConjuncion  = r"/\\"
-#t_TkDisyuncion  = r"\/"
+#t_TkConjuncion  = r'/\\'
+#t_TkDisyuncion  = r'\/'
 t_TkNegacion  = r'not'
 t_TkMenor = r'<'
 t_TkMenorIgual  = r'<='
@@ -94,7 +94,7 @@ t_TkValorAscii  = r'\#'
 t_TkConcatenacion = r'::'
 t_TkShift = r'\$'
 #t_TkCaracter = r'[a-zA-Z]?<![0-9]'
-t_TkCaracter = r'\'[^0-9 ]?\'' #0-9 ni espacio
+t_TkCaracter = r'\'[a-zA-Z]\'|\'[\t]\'|\'[\n]\'|\'[\']\'|\'[\\]\'' #0-9 ni espacio
 
 
 # Regla para identificar numeros
@@ -113,7 +113,8 @@ t_ignore  = ' \t'
 
 # Error handling rule
 def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
+    #Error: Caracter inesperado "!" en la fila 2, columna 20
+    tokError.append("Error: Caracter inesperado \"%s\"" % t.value[0] +" en la fila "+str(lexer.lineno)+", columna "+encontrar_col(data,t))
     t.lexer.skip(1)
 
 def t_TkId(t):
@@ -121,18 +122,46 @@ def t_TkId(t):
     t.type = reserved.get(t.value,'TkId')    # Check for reserved words
     return t
 
-# Build the lexer
-lexer = lex.lex()
+#Para encontrar columna
+def encontrar_col(linea, token):
+    inicio_linea=linea.rfind('\n',0,token.lexpos) + 1
+    return str((token.lexpos - inicio_linea) + 1)
 
 # Test it out
-data = "3 # 4 if not ifi nothing 'a' '\t' ' ' \nalo\nhola%"
+data='''
+    3 # 4 if . true not ifi nothing 'a' '\t' '\n' '\'' '\\' 
+    alo
+    hola%
+    '''
 
+# Build the lexer
+lexer = lex.lex()
 # Give the lexer some input
 lexer.input(data)
 
-# Tokenize
+tokError=[]
+tokens=[]
+
 while True:
     tok = lexer.token()
     if not tok: 
         break      # No more input
-    print(tok)
+
+    if(reserved.get(tok.value)!=None): #Reserved Word
+        tokens.append(tok.type+" "+str(lexer.lineno)+" "+encontrar_col(data,tok))
+    elif(tok.type=='TkId'): #Variables
+        tokens.append(tok.type+"(\""+tok.value+"\") "+str(lexer.lineno)+" "+encontrar_col(data,tok))
+    elif(tok.type=='TkNum'): #Numeros
+        tokens.append(tok.type+"("+str(tok.value)+") "+str(lexer.lineno)+" "+encontrar_col(data,tok))
+    elif(tok.type=='TkCaracter'): #Caracteres
+        tokens.append(tok.type+"("+tok.value+") "+str(lexer.lineno)+" "+encontrar_col(data,tok))
+    else:
+        tokens.append(tok.type+" "+str(lexer.lineno)+" "+encontrar_col(data,tok))
+
+if(len(tokError)>0):
+    for i in range(len(tokError)):
+        print(tokError[i])
+else:
+    for i in range(len(tokens)):
+        print(tokens[i])
+
