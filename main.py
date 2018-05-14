877e6d2d4c97b8c1786323a2661b1581f401c605
prenuevos <prenuevos@Wellick.ac.labf.usb.ve>
2018-05-14 17:24:50 -0400

    Organized the code into separated functions, fixed issue with duplicate list names

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import ply.lex as lex
 import sys
 import re
 
+
+tokError=[] #Global, si esta vacio no hay errores
+tokensList=[]
+
+
 # Lista de nombres de Tokens
 
 reserved={
@@ -92,9 +97,10 @@ t_TkAnteriorCar = r'--'
 t_TkValorAscii  = r'\#'
 t_TkConcatenacion = r'::'
 t_TkShift = r'\$'
-#t_TkCaracter = r'[a-zA-Z]?<![0-9]'
-t_TkCaracter = r'[\'\"][a-zA-Z][\'\"]|[\'\"][\t][\'\"]|[\'\"][\n][\'\"]|[\'\"][\'][\'\"]|[\'\"][\\][\'\"]' #0-9 ni espacio
+t_TkCaracter = r'[\'\"][a-zA-Z0-9][\'\"]|[\'\"][\t][\'\"]|[\'\"][\n][\'\"]|[\'\"][\'][\'\"]|[\'\"][\\][\'\"]' #0-9 ni espacio
 
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
 
 # Regla para identificar numeros
 def t_TkNum(t):
@@ -106,9 +112,6 @@ def t_TkNum(t):
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
-
-# A string containing ignored characters (spaces and tabs)
-t_ignore  = ' \t'
 
 # Error handling rule
 def t_error(t):
@@ -126,41 +129,51 @@ def encontrar_col(linea, token):
     inicio_linea=linea.rfind('\n',0,token.lexpos) + 1
     return str((token.lexpos - inicio_linea) + 1)
 
-# Test it out
-data='''
-    /\\\/ 3 # 4 if . true not ifi nothing 'a' '\t' '\n' '\'' '\\' 
-    alo
-    hola%
-    '''
+def get_especial(tok):
+	if (tok.value=='\'\n\''):
+		return '\''+'\\'+'n'+'\''
+	elif (tok.value=='\'\t\''):
+		return '\''+'\\'+'t'+'\''
+	else:
+		return tok.value
 
-# Build the lexer
-lexer = lex.lex()
-# Give the lexer some input
-lexer.input(data)
+#Para listar el string del token dependiendo de su valor
+def listar_token(tok):
+	if(reserved.get(tok.value)!=None): #Reserved Word
+		tokensList.append(tok.type+" "+str(lexer.lineno)+" "+encontrar_col(data,tok))
+	elif(tok.type=='TkId'): #Variables
+		tokensList.append(tok.type+"(\""+tok.value+"\") "+str(lexer.lineno)+" "+encontrar_col(data,tok))
+	elif(tok.type=='TkNum'): #Numeros
+		tokensList.append(tok.type+"("+str(tok.value)+") "+str(lexer.lineno)+" "+encontrar_col(data,tok))
+	elif(tok.type=='TkCaracter'): #Caracteres
+		tokensList.append(tok.type+"("+get_especial(tok)+") "+str(lexer.lineno)+" "+encontrar_col(data,tok))
+	else:
+		tokensList.append(tok.type+" "+str(lexer.lineno)+" "+encontrar_col(data,tok))
 
-tokError=[]
-tokens=[]
+#Para imprimir la lista de tokens o errores dependiendo del caso
+def print_tokens_or_errors():
+	if(len(tokError)>0):
+	    for i in range(len(tokError)):
+	        print(tokError[i])
+	else:
+	    for i in range(len(tokensList)):
+	        print(tokensList[i])
 
-while True:
-    tok = lexer.token()
-    if not tok: 
-        break      # No more input
+#Lee un archivo y lo retorna como string
+def read_given_file():
+	pass								##############CAMBIAR
+	string=""+sys.argv[1]
+	return string
 
-    if(reserved.get(tok.value)!=None): #Reserved Word
-        tokens.append(tok.type+" "+str(lexer.lineno)+" "+encontrar_col(data,tok))
-    elif(tok.type=='TkId'): #Variables
-        tokens.append(tok.type+"(\""+tok.value+"\") "+str(lexer.lineno)+" "+encontrar_col(data,tok))
-    elif(tok.type=='TkNum'): #Numeros
-        tokens.append(tok.type+"("+str(tok.value)+") "+str(lexer.lineno)+" "+encontrar_col(data,tok))
-    elif(tok.type=='TkCaracter'): #Caracteres
-        tokens.append(tok.type+"("+tok.value+") "+str(lexer.lineno)+" "+encontrar_col(data,tok))
-    else:
-        tokens.append(tok.type+" "+str(lexer.lineno)+" "+encontrar_col(data,tok))
+#main del programa
+def main():
+	lexer=lex.lex()  #build lexer
+	data=read_given_file()   #feed the lexer the input given
+	while True:
+		tok = lexer.token()
+		if not tok: 
+			break      # No more input
+		listar_token(tok)
 
-if(len(tokError)>0):
-    for i in range(len(tokError)):
-        print(tokError[i])
-else:
-    for i in range(len(tokens)):
-        print(tokens[i])
 
+main()
